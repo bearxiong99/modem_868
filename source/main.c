3d040b3533f72772d729a26074d14456f31fee62
Max <t-r-knight@yandex.ru>
2015-08-28 15:38:46 +0300

    Update main.c
    Enable RCC for SPI, TIM, SYSCFG

--- source/main.c
+++ source/main.c
@@ -3,6 +3,7 @@
 
 #include "stm32f4xx.h"
 
+#include "sx1276_cfg.h"
 #include "delay.h"
 #include "led.h"
 
@@ -16,6 +17,9 @@ static void hardware()
                            RCC_AHB1Periph_GPIOC |
                            RCC_AHB1Periph_GPIOD |
                            RCC_AHB1Periph_GPIOE, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 |
+                           RCC_APB2Periph_SYSCFG, ENABLE);
 }
 
 int main()
@@ -24,6 +28,7 @@ int main()
     
     led_init();
     delay_init();
+    sx1276_cfg_init();
     
     xTaskCreate(task_led, "led", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
 
